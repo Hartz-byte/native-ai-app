02692f863fde2d8ab9cd2e522c1de327b5c1aa11
Hartz-byte <harshmail281199@gmail.com>
2024-02-09 15:53:56 +0530

    features update

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 import React from "react";
 import {
   widthPercentageToDP as wp,
@@ -7,8 +7,27 @@ import {
 
 const Features = () => {
   return (
-    <View>
-      <Text>Features</Text>
+    <View style={{ height: hp(60) }} className="space-y-4">
+      <Text
+        style={{ fontSize: wp(6.5) }}
+        className="font-semibold text-gray-700"
+      >
+        Features
+      </Text>
+      <View className="bg-emerald-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            source={require("../../assets/images/chatgptIcon.png")}
+            style={{ height: hp(4), width: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            ChatGPT
+          </Text>
+        </View>
+      </View>
     </View>
   );
 };
