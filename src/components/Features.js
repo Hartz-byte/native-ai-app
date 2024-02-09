4d8e39d1cd32d5428a93e638fbd99a93784c51ab
Hartz-byte <harshmail281199@gmail.com>
2024-02-09 19:37:23 +0530

    update

--- src/components/Features.js
+++ src/components/Features.js
@@ -14,6 +14,8 @@ const Features = () => {
       >
         Features
       </Text>
+
+      {/* feature 1 */}
       <View className="bg-emerald-200 p-4 rounded-xl space-y-2">
         <View className="flex-row items-center space-x-1">
           <Image
@@ -27,6 +29,59 @@ const Features = () => {
             ChatGPT
           </Text>
         </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          ChatGPT can provide you with instant and knowledgeable responses,
+          assist you with creative ideas on a wide range of topics.
+        </Text>
+      </View>
+
+      {/* feature 2 */}
+      <View className="bg-purple-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            source={require("../../assets/images/dalleIcon.png")}
+            style={{ height: hp(4), width: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            DALL-E
+          </Text>
+        </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          DALL-E can generate imaginative and diverse images from textual
+          descriptions, expanding the boundaries of visual creativity.
+        </Text>
+      </View>
+
+      {/* feature 3 */}
+      <View className="bg-cyan-200 p-4 rounded-xl space-y-2">
+        <View className="flex-row items-center space-x-1">
+          <Image
+            source={require("../../assets/images/smartaiIcon.png")}
+            style={{ height: hp(4), width: hp(4) }}
+          />
+          <Text
+            style={{ fontSize: wp(4.8) }}
+            className="font-semibold text-gray-700"
+          >
+            Smart AI
+          </Text>
+        </View>
+        <Text
+          style={{ fontSize: wp(3.8) }}
+          className="text-gray-700 font-medium"
+        >
+          A powerful voice assistant with the abilities of ChatGPT and Dall-E,
+          providing you the best of both worlds.
+        </Text>
       </View>
     </View>
   );
