4aa8f33836387fb0e4a02a74aae65c0f39f3d48d
Hartz-byte <harshmail281199@gmail.com>
2024-02-09 20:04:35 +0530

    dummy messages

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,30 +1,92 @@
-import { Text, View, SafeAreaView, Image } from "react-native";
+import { Text, View, SafeAreaView, Image, ScrollView } from "react-native";
 import React, { useState } from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import Features from "../components/Features";
+import { dummyMessages } from "../constants";
 
 const HomeScreen = () => {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState(dummyMessages);
 
   return (
     <View className="flex-1 bg-white">
       <SafeAreaView className="flex-1 flex mt-16 mx-5">
         {/* bot icon */}
         <View className="flex-row justify-center">
-          <Image source={require("../../assets/images/bot.png")} style={{height: hp(15), width: hp(15)}} />
+          <Image
+            source={require("../../assets/images/bot.png")}
+            style={{ height: hp(15), width: hp(15) }}
+          />
         </View>
 
         {/* features || messages */}
-        {
-          messages.length > 0 ? (
-            <View></View>
-          ) : (
-            <Features />
-          )
-        }
+        {messages.length > 0 ? (
+          <View className="space-y-2 flex-1">
+            <Text
+              style={{ fontSize: wp(5) }}
+              className="text-gray-700 font-semibold ml-1"
+            >
+              Assistant
+            </Text>
+            <View
+              style={{ height: hp(58) }}
+              className="bg-neutral-200 rounded-3xl p-4"
+            >
+              <ScrollView
+                BOUNCES={false}
+                className="space-y-4"
+                showsverticalIndicator={false}
+              >
+                {messages.map((message, index) => {
+                  if (message.role == "assistant") {
+                    if (message.content.includes("https")) {
+                      // its an ai image
+                      return (
+                        <View key={index} className="flex-row justify-start">
+                          <View className="p-2 flex rounded-2xl bg-emerald-100 rounded-tl-none">
+                            <Image
+                              source={{ uri: message.content }}
+                              className="rounded-2xl"
+                              resizeMode="contain"
+                              style={{ height: wp(60), width: wp(60) }}
+                            />
+                          </View>
+                        </View>
+                      );
+                    } else {
+                      // text response
+                      return (
+                        <View
+                          key={index}
+                          style={{ width: wp(70) }}
+                          className="bg-emerald-100 rounded-xl p-2 rounded-tl-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      );
+                    }
+                  } else {
+                    // user input
+                    return (
+                      <View key={index} className="flex-row justify-end">
+                        <View
+                          style={{ width: wp(70) }}
+                          className="bg-white rounded-xl p-2 rounded-tr-none"
+                        >
+                          <Text>{message.content}</Text>
+                        </View>
+                      </View>
+                    );
+                  }
+                })}
+              </ScrollView>
+            </View>
+          </View>
+        ) : (
+          <Features />
+        )}
       </SafeAreaView>
     </View>
   );
